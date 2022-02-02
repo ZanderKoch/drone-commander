a00fdbecd22e7ce7bfccc540cc4a2b47e3a7c099
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-02 20:55:21 +0100

    yeah

---
+++ scrip.js
@@ -0,0 +1,10 @@
+//radar canvas
+let radar = document.querySelector("#radar-canvas");
+let radarCenterX = radar.width / 2;
+let radarCenterY = radar.height/ 2;
+
+//stick canvas
+let stick = document.querySelector("#stick-canvas");
+let stickCenterX = stick.width / 2;
+let stickCenterY = stick.height/ 2;
+
