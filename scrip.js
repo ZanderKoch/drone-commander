6382f9642d49c64604d4ac0efad95f6fa3e22cf9
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-03 13:19:51 +0100

    implemented enough stick functionality for webhook testing

--- scrip.js
+++ scrip.js
@@ -1,10 +1,129 @@
 //radar canvas
-let radar = document.querySelector("#radar-canvas");
-let radarCenterX = radar.width / 2;
-let radarCenterY = radar.height/ 2;
+let radar;
+let radarCx;
+let radarCenterX;
+let radarCenterY;
 
 //stick canvas
-let stick = document.querySelector("#stick-canvas");
-let stickCenterX = stick.width / 2;
-let stickCenterY = stick.height/ 2;
+let stick;
+let stickCx;
+let stickRadius
+let stickCenterX;
+let stickCenterY;
 
+//client state
+let stickGrabbed;
+/**stick's current x coordinate relative to it's center */
+let stickX;
+/**stick's current x coordinate relative to it's center, positive axis is up*/
+let stickY;
+let yaw;
+let throttle;
+
+let clientColor; 
+
+
+function init(){
+    //radar canvas
+    radar = document.querySelector("#radar-canvas");
+    radarCx = radar.getContext("2d");
+    console.log(radar);
+    radarCenterX = radar.width / 2;
+    radarCenterY = radar.height/ 2;
+
+    //stick canvas
+    stick = document.querySelector("#stick-canvas");
+    stickCx = stick.getContext("2d");
+    stickRadius = stick.width / 2;
+    stickCenterX = stick.width / 2;
+    stickCenterY = stick.height/ 2;
+
+    //client state
+    stickGrabbed = false;
+    yaw = 0;
+    throttle = 0;
+
+    clientColor = "#FFFFFF"; //for testing
+
+    //initialize UI
+    //setCanvasSizes();
+    drawStick();
+
+    //event assignment
+    stick.addEventListener("mousedown", e => grabStick(e));
+    stick.addEventListener("mousemove", e => updateStick(e));
+    stick.addEventListener("mouseup", unStick);
+}
+
+function grabStick(event){
+    stickGrabbed = true;
+    updateStick(event);
+}
+
+function updateStick(event){
+    console.log(stickGrabbed);
+    
+    if(stickGrabbed === true){
+        //get stick coordinates relative to stick center
+        stickX = event.offsetX - stickCenterX;
+        stickY = 1-(event.offsetY - stickCenterY);
+
+        console.log("stickX: " + stickX + " ,stickY: " + stickY);
+
+        //calculate throttle and yaw
+        yaw = stickX / stickRadius;
+        throttle = stickY / stickRadius
+        
+        drawStick();
+    }
+}
+
+function unStick(){
+    console.log("let go of stick");
+    if(stickGrabbed === true){
+        stickGrabbed = false;
+    }
+}
+
+//graphics functions//
+
+/**sets the sizes of all canvases to be equal to their parent element*/
+/* function setCanvasSizes(){
+    radar.width = document.getElementById("radar").get;
+} */
+
+function drawStick(){
+    //preparing the context
+    stickCx.resetTransform();
+    stickCx.clearRect(0, 0, stick.width, stick.height);
+    stickCx.strokeStyle = clientColor;
+    stickCx.lineWidth = 1.5;
+    stickCx.translate(stickCenterX, stickCenterY);
+    stickCx.scale(1,-1);
+
+    //drawing a circle to show the usable radar area
+    /* 
+    stickCx.beginPath();
+    stickCx.arc(0, 0, stickRadius, 0, 2 * Math.PI);
+    stickCx.stroke();
+    */
+
+    //drawing a cross (add meausurement lines later)
+    stickCx.beginPath();
+    stickCx.moveTo(0, stickRadius);
+    stickCx.lineTo(0, -stickRadius);
+    stickCx.moveTo(stickRadius, 0);
+    stickCx.lineTo(-stickRadius, 0);
+    stickCx.stroke();
+
+    //drawing a line from the center to the control point
+    console.log("yaw: " + yaw + " throttle: " + throttle)
+    console.log("radius: " + stickRadius)
+    
+    stickCx.beginPath();
+    stickCx.moveTo(0, 0);
+    stickCx.lineTo(yaw * stickRadius, throttle * stickRadius);
+    stickCx.stroke();
+}
+
+window.onload=init;
