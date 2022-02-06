421b3e4f1c5ecf60eef5e595c3aa99c5f169f522
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-06 15:50:10 +0100

    implemented plane variables

--- dronecommander/src/main/java/nu/te4/dronecommander/StateEntities/Plane.java
+++ dronecommander/src/main/java/nu/te4/dronecommander/StateEntities/Plane.java
@@ -7,8 +7,17 @@ package nu.te4.dronecommander.StateEntities;
 public class Plane {
     private float posX;
     private float posY;
-    private float heading; //radians
+    private double heading; //rads
     private String color; //'#' followed by 6 digit hexadecimal
-    private float velocity;
+    
+    private float velocity = 0; //units/timestep
+    private double rotationRate = 0; //rads that heading will change by each timestep
+    
+    //performance variables, ways to change them could be implemented later
+    private double maxRotationRate = Math.toRadians(45);
+    private float engineForce = 10; //multipier for positive throttle
+    private float airbrakeForce = 5; //drag multiplier negative throttle
+    private float dragCoef = 0.1f;
+    
     
 }
