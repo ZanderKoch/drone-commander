cdd745b61224fe2fe841ded730f47dcb6126a806
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-06 17:02:44 +0100

    Update Plane.java

--- dronecommander/src/main/java/nu/te4/dronecommander/StateEntities/Plane.java
+++ dronecommander/src/main/java/nu/te4/dronecommander/StateEntities/Plane.java
@@ -15,9 +15,57 @@ public class Plane {
     
     //performance variables, ways to change them could be implemented later
     private double maxRotationRate = Math.toRadians(45);
-    private float engineForce = 10; //multipier for positive throttle
-    private float airbrakeForce = 5; //drag multiplier negative throttle
+    private double rudder = Math.toRadians(9); //yaw multiplier 
+    private float engine = 10; //multipier for positive throttle
+    private float airbrake = 5; //drag multiplier for negative throttle
     private float dragCoef = 0.1f;
     
+    //latest recieved inputs from client
+    private float yaw = 0;
+    private float throttle = 0;
+
+    public Plane(float posX, float posY, double heading, String color) {
+        this.posX = posX;
+        this.posY = posY;
+        this.heading = heading;
+        this.color = color;
+    }
+    
+    /**
+     * updates plane's yaw value while ensuring -1 <= yaw <= 1
+     * @param yawInput
+     * @return plane's updated yaw
+     */
+    public float updateYaw(float yawInput){
+        if(yawInput > 1){
+            this.yaw = 1;
+        }
+        else if(yawInput < -1){
+            this.yaw = -1;
+        }
+        return this.yaw;
+    }
+    
+    /**
+     * updates plane's throttle value while ensuring -1 <= throttle <= 1
+     * @param throttleInput
+     * @return plane's updated throttle
+     */
+    public float updateThrottle(float throttleInput){
+        if(throttleInput > 1){
+            this.yaw = 1;
+        }
+        else if(throttleInput < -1){
+            this.yaw = -1;
+        }
+        return this.throttle;
+    }
+    
+    /**
+     * updates the plane's state into the next timestep
+     */
+    public void update(){
+        
+    }
     
 }
